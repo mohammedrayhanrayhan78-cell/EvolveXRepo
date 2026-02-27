40e67ac4e7a0e661ec7c40e24fbabead0416024e
F1 <mohammedrayhanrayhan78-cell>
2026-02-27 07:17:46 +0530

    Updated Day_1.java

--- DAY_1.java
+++ DAY_1.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Scanner;
 
 public class DAY_1 {
@@ -18,5 +19,27 @@ public class DAY_1 {
         scan.close();
     }
 }
+*/
+import java.util.Scanner;
+
+public class DAY_1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your AGE: ");
+        int Age = scan.nextInt();
+        if(Age>=18){
+            System.out.println("You are MAJOR and you can now VOTE");
+        }
+        else if(Age<18&&Age>0){
+            System.out.println("You are still a CHILD come back later when you grow up ");
+        }
+        else{
+            System.out.println("NOT a VALID age");
+        }
+        scan.close();
+    }
+}
+
 
 
