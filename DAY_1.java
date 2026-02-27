64483c5e07f645c1abf6774dcb925c58f640f4f8
F1 <mohammedrayhanrayhan78-cell>
2026-02-27 07:35:48 +0530

    Update DAY_1.java

--- DAY_1.java
+++ DAY_1.java
@@ -19,8 +19,9 @@ public class DAY_1 {
         scan.close();
     }
 }
-*/
+
 import java.util.Scanner;
+
 
 public class DAY_1 {
 
@@ -40,6 +41,30 @@ public class DAY_1 {
         scan.close();
     }
 }
+*/
+import java.util.Scanner;
 
+public class DAY_1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter two number to find out largest amongst them : ");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        if(x>y){
+            System.out.println("X is greatest");
+        }
+        else if(x<y){
+            System.out.println("Y is greatest");
+        }
+        else if(x==y){
+            System.out.println("X and Y are equal");
+        }
+        else{
+            System.out.println("NOT a VALID number");
+        }
+        scan.close();
+    }
+}
 
 
