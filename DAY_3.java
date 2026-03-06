d2ee2602016f86c1ae7099769965eea1e933f680
F1 <mohammedrayhanrayhan78-cell>
2026-03-06 06:36:45 +0530

    DAY_4 code

---
+++ DAY_3.java
@@ -0,0 +1,53 @@
+//import java.util.Scanner;
+//public class DAY_3 {
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        int i , W = 0,tW = 0;
+//        System.out.print("Enter the number of DAYS: ");
+//        int N = scan.nextInt();
+//        for(i = 1 ; i <= N ; i++){
+//            System.out.print("Enter how many STEPS you have WALKED on the DAY " + i + ":");
+//            W = scan.nextInt();
+//            tW = tW + W;
+//        }
+//        System.out.println("The DAY is " + i + "and the STEPS are " + tW);
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_3{
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter the NUMBER of whose you want the elements: ");
+//        int N = scan.nextInt();
+//        for(int i = 1 ; i <= 10; i++){
+//            System.out.println(i*N);
+//        }
+//        System.out.println("END OF THE TABLE");
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_3{
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        int tN = 0;
+//        System.out.print("Enter the NUMBER upto which you want the SUM OF NATURAL NUMBERS: ");
+//        int N = scan.nextInt();
+//        for(int i = 0;i <= N; i++){
+//            tN = tN + i;
+//            System.out.println(tN);
+//        }
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_3{
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        int tN = 1 ;
+//        System.out.print("Enter the NUMBER upto which you want the PRODUCT OF NATURAL NUMBERS: ");
+//        int N = scan.nextInt();
+//        for(int i = 1;i <= N; i++){
+//            tN = tN * i;
+//            System.out.println(tN);
+//        }
+//    }
+//}
