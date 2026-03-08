18aa53c7423a9a6808da5c099234478518f7f64a
F1 <mohammedrayhanrayhan78-cell>
2026-03-08 18:03:39 +0530

    DAY_5 code

---
+++ DAY_4.java
@@ -0,0 +1,48 @@
+//import java.util.Scanner;
+//public class DAY_4{
+//    public static void main(String[]args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter the number from where you want the COUNTDOWN: ");
+//        int N = scan.nextInt();
+//        for(int i = N;i>=0;i--){
+//            System.out.println(i);
+//        }
+//System.out.println("The ROCKET is launched");
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_4{
+//    public static void main(String[]args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter the number of BOTTLES you want: ");
+//        int N = scan.nextInt();
+//        for(int i=0;i<=N;i++){
+//            System.out.println("The BOTTLE " + i + " is filled");
+//        }
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_4{
+//    public static void main(String[]args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter a POSITIVE INTEGER: ");
+//        int N = scan.nextInt();
+//        long factorial = 1;
+//        for(int i=1;i<=N;i++){
+//           factorial*=i;
+//        }
+//        System.out.println("The FACTORAIAL " + N + " is " + factorial);
+//    }
+//}
+//import java.util.Scanner;
+//public class DAY_4{
+//    public static void main(String[]args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter a NUMBER: ");
+//        int N = scan.nextInt();
+//        System.out.println("EVEN NUMBERS from 1 to " + N + " :");
+//        for(int i=2;i<=N;i+=2){
+//            System.out.println(i);;
+//        }
+//    }
+//}
